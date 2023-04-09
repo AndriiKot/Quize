c9c326d3081d697d191918187c34e0c0e7367337
AndriiKot <a_kocyuba@ukr.net>
2023-04-09 23:02:23 +0200

    Creation callback in Ruby

---
+++ main1.rb
@@ -0,0 +1,40 @@
+﻿def copyArrayDoSmth(arr,instructions)
+   output = []
+   arr.size.times { |i| output.push(instructions[arr[i]])}
+   output
+end
+
+class Instr
+  def add(arg) = arg + arg
+  def squar(arg) = arg * arg
+end
+
+instructions = Instr.new
+instructionsAdd = instructions.method(:add)
+instructionsSquar = instructions.method(:squar)
+
+
+p instructionsAdd
+p instructionsSquar
+
+arr = *1..10
+
+newArr1 = copyArrayDoSmth(arr,instructionsAdd)
+newArr2 = copyArrayDoSmth(arr,instructionsSquar)
+
+p arr
+p newArr1
+p newArr2
+
+
+
+
+
+
+
+
+
+
+
+
+
