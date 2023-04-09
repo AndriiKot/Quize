37575cd22ed39cab994a9aa32818b41e774f85ac
AndriiKot <a_kocyuba@ukr.net>
2023-04-10 00:01:56 +0200

    Creation callback in Ruby version 2 (proc)

---
+++ main3.rb
@@ -0,0 +1,31 @@
+﻿def copyArrayDoSmth(arr,instructions)
+   output = []
+   arr.size.times { |i| output.push(instructions[arr[i]])}
+   output
+end
+
+add = proc {|arg| arg + arg}
+squar = proc {|arg| arg * arg}
+
+
+arr = *1..5
+
+newArr1 = copyArrayDoSmth(arr,add)
+newArr2 = copyArrayDoSmth(arr,squar)
+
+p arr
+p newArr1
+p newArr2
+
+
+
+
+
+
+
+
+
+
+
+
+
