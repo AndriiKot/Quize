f5ba1ead5e36447fa11c1115069046c12c08ed04
AndriiKot <AndriiKot@users.noreply.github.com>
2025-03-09 11:49:48 +0100

    Game: 'Quize' 'Ruby 3' ! feate: setup testing 1 !

--- quiz-ruby.rb
+++ quiz-ruby.rb
@@ -10,43 +10,44 @@ existing_keys = File.exist?(repeat_path) ? JSON.parse(File.read(repeat_path)).ke
 events = {
   game: {
     start: {
-      repeat: ->(key, value) { puts value },
-      skip: -> { puts "You pressed Space: Moving to the next question..." },
+      repeat: ->(key, value) { p 'REPEAT ', key, value },
+      skip: -> { puts "SKIP" },
     },
     end: {},
     exit: {},
+    message: {},
 },
   error: {},
+  libs: {},
   utils: {},
-  paths: {},
-  load: {
-    libs: {},
-    data: {},
-  },
+  data: {},
+  setup: -> { p 'HI! '},
 }
 
-  "exit?" => ->(key) { exit if key.casecmp? 'q' },
-  "repeat" => ->(key, value) { puts 'You pressed Enter: continuing...',events},
-  "skip" => -> { puts "You pressed Space: Moving to the next question..." },
-  "repeat_condition?" => ->(arg) { arg == "\r" || arg == "\n" },
-  "skip_condition?" => ->(arg) { arg == " " },
-  "save" => ->(key, value) { File.open(repeat_path, 'a')},
-  "save_condition?" => ->(key) { existing_keys.include? key },
-  "open_file_to_add" => ->(key, value) {},
-  "add_to_repeat" => ->(key, value) {},
-}
+events[:setup].call
+
+#   "exit?" => ->(key) { exit if key.casecmp? 'q' },
+#   "repeat" => ->(key, value) { puts 'You pressed Enter: continuing...',events},
+#   "skip" => -> { puts "You pressed Space: Moving to the next question..." },
+#   "repeat_condition?" => ->(arg) { arg == "\r" || arg == "\n" },
+#   "skip_condition?" => ->(arg) { arg == " " },
+#   "save" => ->(key, value) { File.open(repeat_path, 'a')},
+#   "save_condition?" => ->(key) { existing_keys.include? key },
+#   "open_file_to_add" => ->(key, value) {},
+#   "add_to_repeat" => ->(key, value) {},
+# }
 
 
-data.each do |arr|
-  question, answer = arr[0], arr[1]
+# data.each do |arr|
+#   question, answer = arr[0], arr[1]
 
-  puts "Press 'q' to exit:"
-  puts "Press 'Enter' to repeat or 'Space' to continue"
-  puts "Question: #{question}"
+#   puts "Press 'q' to exit:"
+#   puts "Press 'Enter' to repeat or 'Space' to continue"
+#   puts "Question: #{question}"
 
-    key = STDIN.getch
-  events['exit?'][key]
+#     key = STDIN.getch
+#   events['exit?'][key]
 
-  events['repeat'].call(question, answer) if events['repeat_condition?'][key]
-  events['skip'].call  if events['skip_condition?'][key]
-end
+#   events['repeat'].call(question, answer) if events['repeat_condition?'][key]
+#   events['skip'].call  if events['skip_condition?'][key]
+# end
